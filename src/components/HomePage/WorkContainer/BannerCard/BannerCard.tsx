946f6007e4642f104ca28758a8ce17647e40cc21
yuzhen <caiyuzhen00@gmail.com>
2022-10-27 13:30:35 +0800

    build article

--- src/components/HomePage/WorkContainer/BannerCard/BannerCard.tsx
+++ src/components/HomePage/WorkContainer/BannerCard/BannerCard.tsx
@@ -1,6 +1,8 @@
 import React, { FC } from 'react'
 import './BannerCard.less'
 import mainWork from '../../../../assets/img/work-1.jpg'
+import detailIcon from '../../../../assets/svg/icon-detail.svg'
+
 
 export const BannerCard:FC = () => {
 	return (
@@ -10,6 +12,15 @@ export const BannerCard:FC = () => {
 				<div className="content-conatiner">
 					<p className="content-title">UI&UX Design</p>
 					<p className="content-subtitle">A simple note app, some desctiptions, and some examples of how to use it.A simple note app, some desctiptions, and some examples of how to use it.</p>
+					<div className="navDot-container">
+						<div className="navDotGroup">
+							<span className="dot "></span>
+							<span className="dot dot-Selector"></span>
+							<span className="dot "></span>
+							<span className="dot "></span>
+						</div>
+						<img src={detailIcon} alt="" />
+					</div>
 				</div>
 			</div>
 		</>
