a965e1d1faa5e56c996dff2871dfa48012759924
Caiyuzhen <caiyuzhen00@gmail.com>
2023-04-06 01:38:28 +0800

    update

--- src/components/HomePage/WorkContainer/pages/VcStructure/VcStructure.tsx
+++ src/components/HomePage/WorkContainer/pages/VcStructure/VcStructure.tsx
@@ -14,27 +14,29 @@ export const VcStructure = () => {
 			<div className="VcStructure-main-container">
 				<div className="VcStructure-inside-container">
 					<div className="VcStructure-top-document">
-						<div className="VcStructure-top-circle">Based on presentation topicsConvert to structured document</div>
-						<img src={gradualArrow} alt="" className="gradualArrow-right"/>
-						<img src={catalogCard} alt="" className="video-catalogCard"/>
-						{/* 右虚线 */}
-						<div className="VcStructure-trianArrowRight-right">
+						<div className="VcStructure-top-circle">Based on presentation topics convert to structured document</div>
+										{/* 右虚线 */}
+										<div className="VcStructure-trianArrowRight-right">
 							<span className="dotted-line-right"></span>
 							<span className="trianArrow-right-01"></span>
 						</div>
+						
+						<img src={catalogCard} alt="" className="video-catalogCard"/>
+						<img src={gradualArrow} alt="" className="gradualArrow-right"/>
 						<img src={videoDoc} alt="" className="video-doc"/>
 					</div>
 
 
 					<div className="VcStructure-bottom-timeline">
-						<div className="VcStructure-bottom-circle">Video-based collateral information Conversion to structured tables</div>
-						<img src={gradualArrow} alt="" className="gradualArrow-right"/>
-						<img src={timeLine} alt="" className="video-timeLine"/>
+						<div className="VcStructure-bottom-circle">Video-based collateral info conversion to structured tables</div>
 						{/* 右虚线 */}
 						<div className="VcStructure-trianArrowRight-right">
 							<span className="dotted-line-left"></span>
 							<span className="trianArrow-left-02"></span>
 						</div>
+						
+						<img src={timeLine} alt="" className="video-timeLine"/>
+						<img src={gradualArrow} alt="" className="gradualArrow-right"/>
 						<img src={videoLine} alt="" className="video-structure"/>
 					</div>
 
